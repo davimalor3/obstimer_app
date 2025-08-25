9b3c27a614a4cef5f1728286e4ff561b32dd5633
Davi Nobre <davi.nbtec@gmail.com>
2025-08-25 02:22:04 -0300

    Create GUI OBS and edit Main

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-import time
+from ttkbootstrap import Window
 
-from obswebsocket import obsws, requests
+from app.main_obs_ui import OBSTimerApp
 
-HOST = "192.168.0.100"
-PORT = 4455
-PASSWORD = "NoslCIOYUUQa2dNN"
-TEMPO_DE_GRAVACAO = 5
 
-# Conectar ao OBS WebSocket
-ws = obsws(HOST, PORT, PASSWORD)
-ws.connect()
+def main():
+    root = Window(themename="darkly")
+    app = OBSTimerApp(root)
+    root.mainloop()
 
-# Iniciar gravação
-print("Iniciando gravação...")
-ws.call(requests.StartRecord())
 
-# Esperar o tempo definido
-time.sleep(TEMPO_DE_GRAVACAO)
-
-# Parar gravação
-print("Parando gravação...")
-ws.call(requests.StopRecord())
-
-# Desconectar
-ws.disconnect()
-print("Gravação finalizada com sucesso!")
+if __name__ == "__main__":
+    main()
